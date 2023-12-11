fcd5e39f198eae3bc657957f90636b828b46c60b
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-11 15:32:53 +0000

    [Add] user User authentication function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # resourceという引数には、ログインを実行したモデルのデータ、今回の場合はつまりログインしたUserのインスタンスが格納
+  # ログイン後にマイページなどのログインしたユーザーに関連するようなページ遷移を実装したい場合は、このresourceを活用して実装
+  # Home画面？
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
+  # ログイン画面？
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # devise を使用する際に URL として users を含むことを示す
+  devise_for :users
   root to: "homes#top"
+  get 'homes/about' => 'homes#about', as: 'about'
 end
