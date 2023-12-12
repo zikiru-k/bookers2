2fd4eefc0da27e974d41ff7126be7c941e19f43d
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-12 04:54:05 +0000

    [Add] Create posting function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,24 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    # Book detail 詳細画面に変更する
+    redirect_to book_path
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :Opinion)
   end
 end
--- db/migrate/20231212011755_create_books.rb
+++ db/migrate/20231212011755_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :body
+      t.string :Opinion
       t.integer :user_id
       t.timestamps
     end
