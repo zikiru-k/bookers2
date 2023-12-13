c5d43db17b43a5ff3b7f6d95d4ac8a5eeb7bcc73
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-13 01:43:32 +0000

    [Add] Setting profile image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :Opinion)
+    params.require(:book).permit(:title, :opinion)
   end
 end
