3286e53e71d8dc69039521bee3a357513dac0e58
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-13 01:31:40 +0000

    [Add] User information editing function

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2023_12_12_011755) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "Opinion"
+    t.string "opinion"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
