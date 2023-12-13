bc31d9942ca25b87020346c813099fc3539ff07b
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-13 10:39:24 +0000

    [Fix] Displaying a list of user profile images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
+  
+  def index
+    @users = User.all
+  end
 
   def edit
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '/homes/about' => 'homes#about', as: 'about'
 
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update]
 end
