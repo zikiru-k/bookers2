5746e21dedd3cff3747190cdd6d077ea8cc01b73
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-13 02:23:22 +0000

    [Add] Book details screen

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,10 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.delete
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get '/homes/about' => 'homes#about', as: 'about'
 
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
